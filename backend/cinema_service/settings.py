4d4d755b12c21acc46b867fac899c380cb77ae30
Ruslan Kazmiryk <callogan217@gmail.com>
2024-05-23 14:35:47 +0300

    Preliminary commit

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +31,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  #"127.0.0.1:5173"
 
 INTERNAL_IPS = [
     "127.0.0.1",
